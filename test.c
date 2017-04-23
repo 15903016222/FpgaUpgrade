692e17a10da3abe6007c9d68a528f3f3095cca71
王 胜 <wangsheng@cndoppler.cn>
2017-04-23 09:41:23 +0800

    读取文件，写入spi flash

--- test.c
+++ test.c
@@ -1,8 +1,9 @@
 #include "spi.h"
 
-static int fd_mtd, fd_tt;
+#define SIZE 4096
 
 int spi_mode (void) {
+    int fd_mtd, fd_tt;
     int res, val;
     static uint8_t mode;
     static uint8_t bits = 8;
@@ -48,31 +49,65 @@ int spi_mode (void) {
     return 0;
 }
 
-int main (int argc, char *argv[]) {
-    int i, j;
+int file_operate (const char *path, size_t *size)
+{
+    int fd;
 
+    if (NULL == path || NULL == size)
+    {
+        return -1;
+    }
+
+    fd = open (path, O_RDONLY);
+    if (fd < 0)
+    {
+        return -1;
+    }
+    *size = lseek(fd, 0, SEEK_END);
+    if (NULL == size)
+    {
+        return -1;
+    }
+    lseek(fd, 0, SEEK_SET);
+
+    return fd;
+}
+
+void spi_wait_ready (void)
+{
+    while (1) {
+
+        if (!spi_is_busy())
+        {
+            break;
+        }
+        else
+        {
+            printf ("spi is busy ... \n");
+            sleep (5);
+            continue;
+        }
+    }
+
+    return ;
+}
+
+int main (int argc, char *argv[])
+{
     int fd_file;
+    int res, tmp;
+    unsigned int addr = 0;
     size_t size = 0;
-    size_t tmp = 0;
-    char buff[256] = {0};
+    char buff[SIZE] = {0,};
 
     spi_mode();
 
-/*
-    // file operation
-    fd_file = open (argv[1], O_RDONLY);
-    if (fd_file < 0) {
-        perror ("open");
+    fd_file = file_operate (argv[1], &size);
+    if (fd_file)
+    {
+        perror ("file_operate");
         return -1;
     }
-    size = lseek(fd_file, 0, SEEK_END);
-    if (size < 0) {
-        return -1;
-    }
-    lseek(fd_file, 0, SEEK_SET);
-
-    printf ("file: size = %d \n", size);
-*/
 
     // RDID
     char id[3] = {0,};
@@ -80,95 +115,48 @@ int main (int argc, char *argv[]) {
     printf ("ID:id = %.2x %.2x %.2x \n", id[0], id[1], id[2]);
 
     // RDSR
-
     unsigned char status;
     spi_rdsr(&status);
     printf ("RDSR: status = %.2x \n", status);
 
-
-    // SE
-//    spi_se(0x300);
-
+    spi_wait_ready();
     // BE
     spi_be();
 
-    // RDSR
-    while (1) {
-
-        if (!spi_is_busy()) {
-            break;
-        }
-        else {
-            printf ("RDSR: status is busy \n");
-            sleep (5);
-            continue;
-        }
-    }
-
-    // PP
-/*    char write_data[256] = {0};
-
-    for (i = 0; i < 256; ++i) {
-        write_data[i] = i % 256;
-    }
-    printf ("write_data: \n");
-    for (i = 0; i < 8; ++i) {
-        for (j = 0; j < 32; ++j) {
-            printf ("%.2x ", write_data[i * 16 + j]);
-        }
-        printf ("\n");
-    }
-
-    spi_pp(0x200, write_data, 256);
-*/
-
-    // BP
-    char write_data[1024 + 256] = {0};
-
-    for (i = 0; i < 1024 + 256; ++i) {
-        write_data[i] = i % 256;
-    }
-    printf ("write_data: \n");
-    for (i = 0; i < 40; ++i) {
-        for (j = 0; j < 32; ++j) {
-            printf ("%.2x ", write_data[i * 32 + j]);
-        }
-        printf ("\n");
-        if (i == 32)
+    tmp = size;
+    while (tmp / SIZE)
+    {
+        memset (buff, 0, SIZE);
+        // read file
+        if ((res = read (fd_file, buff, SIZE)) < 0)
         {
-            printf ("\n");
+            perror ("read");
+            return -1;
         }
+        // write to flash
+        spi_wait_ready();
+        res = spi_write(addr, buff, res);
+        addr += res;
+        tmp  -= res;
     }
 
-    spi_write(0x0, write_data, 1024 + 128);
-
-    // RDSR
-    while (1) {
-
-        if (!spi_is_busy()) {
-            break;
-        }
-        else {
-            printf ("RDSR: status is busy \n");
-            sleep (5);
-            continue;
-        }
-    }
-
-    // READ
-    char read_data[2048] = {0};
-    spi_read(0x0, read_data, sizeof (read_data));
-    printf ("READ: \n");
-    for (i = 0; i < 64; ++i) {
-        for (j = 0; j < 32; ++j) {
-            printf ("%.2x ", read_data[32 * i + j]);
-        }
-        printf ("\n");
-        if (i == 31)
+    while (tmp % SIZE != 0)
+    {
+        memset (buff, 0, SIZE);
+        // read file
+        if ((res = read (fd_file, buff, tmp % SIZE)) < 0)
         {
-            printf ("\n");
+            perror ("read");
+            return -1;
         }
+        // write to flash
+        spi_wait_ready();
+        res = spi_write(addr, buff, res);
+        addr += res;
+        tmp  -= res;
     }
+
+    spi_wait_ready();
 
     return 0;
 }
